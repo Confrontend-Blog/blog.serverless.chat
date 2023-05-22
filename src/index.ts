52358c197fd53e95982f99517c34581377904329
Hamed-Fatehi <hamed.fatehi@jambit.com>
2023-05-22 21:29:07 +0200

    Add firebase token signin

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,27 @@ export * from "./utils/string-utils";
 
 import { FirebaseApp, FirebaseOptions, initializeApp } from "firebase/app";
 import { Firestore, getFirestore } from "firebase/firestore";
+import { getAuth, signInWithCustomToken } from "firebase/auth";
 
 export let db: Firestore;
 export let app: FirebaseApp;
 
-export function initializeFirebase(config: FirebaseOptions) {
+export function initializeFirebase(
+  config: FirebaseOptions,
+  firebaseToken: string
+) {
   app = initializeApp(config);
   db = getFirestore(app);
+  const auth = getAuth();
+
+  signInWithCustomToken(auth, firebaseToken).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.error(
+      "Error signing in with custom token:",
+      errorCode,
+      errorMessage
+    );
+  });
 }
